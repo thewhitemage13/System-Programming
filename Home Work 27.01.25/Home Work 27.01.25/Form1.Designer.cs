1ae733171b870f3bd9a73acae6fe15d487783e63
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2025-01-28 11:38:00 +0100

    Add files via upload

---
+++ Home Work 27.01.25/Home Work 27.01.25/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Home_Work_27._01._25
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            startButton = new Button();
+            resultLabel = new Label();
+            SuspendLayout();
+            // 
+            // startButton
+            // 
+            startButton.Location = new Point(217, 368);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(321, 70);
+            startButton.TabIndex = 0;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += StartButton_Click;
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(611, 43);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(39, 15);
+            resultLabel.TabIndex = 2;
+            resultLabel.Text = "Result";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(resultLabel);
+            Controls.Add(startButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button startButton;
+        private Label resultLabel;
+    }
+}
---
+++ Home Work 27.01.25/Home Work 27.01.25/Form1.cs
@@ -0,0 +1,146 @@
+﻿namespace Home_Work_27._01._25
+{
+    /// <summary>
+    /// Клас, що представляє головну форму додатку з гоночним симулятором.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Список для зберігання прогрес-барів, які представляють коней у гонці.
+        /// </summary>
+        private List<ProgressBar> progressBars = new List<ProgressBar>();
+
+        /// <summary>
+        /// Кількість коней, які беруть участь у гонці.
+        /// </summary>
+        private const int NumberOfHorses = 5;
+
+        /// <summary>
+        /// Генератор випадкових чисел для моделювання швидкості коней.
+        /// </summary>
+        private Random random = new Random();
+
+        /// <summary>
+        /// Список потоків для кожного коня.
+        /// </summary>
+        private List<Thread> horseThreads = new List<Thread>();
+
+        /// <summary>
+        /// Конструктор форми. Додає прогрес-бари та ініціалізує компоненти форми.
+        /// </summary>
+        public Form1()
+        {
+            AddProgressBar();
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Додає прогрес-бари для кожного коня до форми.
+        /// </summary>
+        private void AddProgressBar()
+        {
+            for (int i = 0; i < NumberOfHorses; i++)
+            {
+                var progressBar = new ProgressBar
+                {
+                    Name = $"Horse{i + 1}",
+                    Minimum = 0,
+                    Maximum = 100,
+                    Value = 0,
+                    Location = new Point(50, 30 + i * 50),
+                    Size = new Size(400, 30)
+                };
+                progressBars.Add(progressBar);
+                this.Controls.Add(progressBar);
+            }
+        }
+
+        /// <summary>
+        /// Обробник події для кнопки старту. Запускає гонку.
+        /// </summary>
+        /// <param name="sender">Джерело події.</param>
+        /// <param name="e">Дані події.</param>
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            resultLabel.Text = "Гонка триває...";
+            horseThreads.Clear();
+
+            List<(int horse, int time)> results = new List<(int horse, int time)>();
+            object lockObject = new object();
+
+            for (int i = 0; i < NumberOfHorses; i++)
+            {
+                int horseIndex = i;
+                Thread thread = new Thread(() =>
+                {
+                    try
+                    {
+                        int timeTaken = RunRace(horseIndex);
+                        lock (lockObject)
+                        {
+                            results.Add((horseIndex + 1, timeTaken));
+                            if (results.Count == NumberOfHorses)
+                            {
+                                Invoke(new Action(() => DisplayResults(results)));
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Invoke(new Action(() => MessageBox.Show($"Помилка: {ex.Message}")));
+                    }
+                });
+                thread.IsBackground = true;
+                horseThreads.Add(thread);
+                thread.Start();
+            }
+        }
+
+        /// <summary>
+        /// Моделює гонку для одного коня.
+        /// </summary>
+        /// <param name="horseIndex">Індекс коня у списку.</param>
+        /// <returns>Час, за який кінь фінішував.</returns>
+        private int RunRace(int horseIndex)
+        {
+            int progress = 0;
+            int totalTime = 0;
+
+            while (progress < 100)
+            {
+                int speed = random.Next(1, 5);
+                progress += speed;
+                totalTime++;
+
+                Invoke(new Action(() =>
+                {
+                    if (progressBars[horseIndex].IsDisposed) return;
+                    progressBars[horseIndex].Value = Math.Min(progress, 100);
+                }));
+
+                Thread.Sleep(50);
+            }
+
+            return totalTime;
+        }
+
+        /// <summary>
+        /// Відображає результати гонки після її завершення.
+        /// </summary>
+        /// <param name="results">Список результатів кожного коня.</param>
+        private void DisplayResults(List<(int horse, int time)> results)
+        {
+            results.Sort((x, y) => x.time.CompareTo(y.time));
+
+            string resultText = "Результати:\n";
+            for (int i = 0; i < results.Count; i++)
+            {
+                resultText += $"{i + 1}. Кінь {results[i].horse} - {results[i].time} одиниць\n";
+            }
+
+            resultLabel.Text = resultText;
+            startButton.Enabled = true;
+        }
+    }
+}
